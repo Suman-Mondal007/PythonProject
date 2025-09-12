a58834b73f781ae808455b741c5b0db22d078402
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-12 18:30:46 +0530

    simple calculator

---
+++ python journey/p3.py
@@ -0,0 +1,11 @@
+#simple calculator
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("sum:",c)
+print("div:",d)
+print("mul:",e)
+print("div:",f)
