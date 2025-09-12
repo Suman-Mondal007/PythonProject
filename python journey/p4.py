f0b33fb80499bc1a4aed3eceb44d3ebbffc0e8dc
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-12 18:40:49 +0530

    simple area calculator

---
+++ python journey/p4.py
@@ -0,0 +1,17 @@
+print("enter the value of two side:")
+side1=int(input("side1:"))
+side2=int(input("side2:"))
+area=side1*side2
+print("enter the value of square side:")
+side3=int(input("side:"))
+area2=side3*side3
+print("enter the value of circle radius:")
+side5=int(input("radius:"))
+area3=3.14*side5*side5
+print("enter the value of triangle side:")
+side4=int(input("height:"))
+side6=int(input("base:"))
+area4=0.5*side4*side6
+print("area of a rectangle:",area)
+print("area of a square:",area2)
+print("area of a circle:",area3)
