d9813721b7d164919762383c5b877801793816ab
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-12 21:32:04 +0530

    result using if-else

---
+++ python journey/p-09.py
@@ -0,0 +1,11 @@
+a=int(input("Enter your marks:"))
+if(a>=90 and a<=100):
+    print("Grade:'A'")
+elif(a>=80 and a<=89):
+    print("Grade:'B'")
+elif(a>=70 and a<=79):
+    print("Grade:'C'")
+elif(a>=35 and a<=69):
+    print("Grade:'D'")
+else:
+    print("fail")
