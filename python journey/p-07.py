6113d1a1fc3c5bcb5d1032b61e0c01c39e32f102
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-12 21:10:33 +0530

    all about strings

---
+++ python journey/p-07.py
@@ -0,0 +1,31 @@
+str1="suman"
+str2="Mondal"
+str3=str1+str2
+print(str3)
+
+str4="Adamas University"
+print(len(str4))
+
+str5="Adamas University"
+ch1=str5[4]
+print(ch1)
+
+str6="Adamas University"
+ch2=str6[4:6] #one less then from the last index
+print(ch2)
+
+str6="Adamas University"
+ch3=str6[-4:-2] #one less then from the last index
+print(ch3)
+
+str7="Adamas University"
+print(str7.endswith("ty"))  #if ends then true otherwise false
+
+str8="adamas University"
+print(str8.capitalize())
+
+str9="Adamas University"
+print(str9.replace("Ada","sum"))
+
+str10="Adamas University"
+print(str10.count("a"))  #count the occurance
