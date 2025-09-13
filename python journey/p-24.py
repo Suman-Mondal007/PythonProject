49e3928bcfa7b12be076b974c4f682e5bbd971fc
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 11:11:30 +0530

    all about for loop

---
+++ python journey/p-24.py
@@ -0,0 +1,14 @@
+nums=[1,4,9,16,25,36,49,64,81,100,49]
+x=49
+index=0
+for el in nums:
+    if(el==x):
+        print("number found at index",index)
+    index +=1
+
+
+for i in range(2,101,2):  #even number
+      print(i)
+
+for i in range(1,101,2):  #odd number
+      print(i)
