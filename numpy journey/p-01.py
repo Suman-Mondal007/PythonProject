26f694a98c399e77c1280486115247b04e307570
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 19:53:31 +0530

    simple numpy

---
+++ numpy journey/p-01.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr=np.array([1,2,3])
+print(arr)   #1-d array
+
+arr2=np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+print(arr2) #2d array
+
+
+matrix=np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]])
+print(matrix) #  simple matrix
