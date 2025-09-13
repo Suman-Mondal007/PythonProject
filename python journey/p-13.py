8b34781a7a50b5361e8d1f4205b8bb559287dfb6
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 07:59:54 +0530

    all about list

---
+++ python journey/p-13.py
@@ -0,0 +1,36 @@
+list=[94, 85, 65, 45, 76]
+print(list)
+
+list=[94, 85, 65, 45, 76]
+print(len(list))
+
+list=[94, 85, 65, 45, 76]
+list.append(55)
+print(list)
+
+list=[94, 85, 65, 45, 76]
+print(list[1:4])
+
+list=[94, 85, 65, 45, 76]
+list.sort()
+print(list)  #accending order
+
+list=[94, 85, 65, 45, 76]
+list.sort(reverse=True)  #decending order
+print(list)
+
+list=[94, 85, 65, 45, 76]
+list.reverse()
+print(list)
+
+list=[94, 85, 65, 45, 76]
+list.insert(1,25)
+print(list)
+
+list=[94, 85, 65, 45, 76]
+list.pop(3)
+print(list)
+
+list=[94, 85, 65, 45, 76]
+list.clear()
+print(list)
