572b2e13744be4253fd1f3c15c2c9038e9abeb28
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 20:04:39 +0530

    simple numpy part-2

---
+++ numpy journey/p-02.py
@@ -0,0 +1,22 @@
+#filled with 0
+import numpy as np
+arr=np.zeros((2,3))
+print(arr)
+
+
+#filled with 1
+arr2=np.ones((2,3))
+print(arr2)
+
+#filled with any default values
+arr3=np.full((2,3),4)
+print(arr3)
+
+#creating a sequence of number
+arr4=np.arange(1,10,2)  #(start,stop,step)
+print(arr4)
+
+#identity matrices
+arr5=np.eye(3)
+print(arr5)
+
