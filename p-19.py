23c9700f4f96febbb145db8e341769741c13824e
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 09:31:55 +0530

    all about set

--- p-19.py
+++ p-19.py
@@ -14,4 +14,5 @@ print(collections)
 set1={1,2,3,4}
 set2={3,4,5,6}
 print(set1.union(set2))  #union
-print(set1.intersection(set2))  #intersection
+print(set1.intersection(set2)) #intersection
+print(set1)
