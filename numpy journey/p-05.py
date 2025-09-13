0eda51f84095e184469dc0e79286b4e4aacd2637
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-09-13 22:01:21 +0530

    simple aggregate function

---
+++ numpy journey/p-05.py
@@ -0,0 +1,11 @@
+#simple aggregation function
+import numpy as np
+
+arr=np.array([1,2,3,4,5])
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.median(arr))
+print(np.std(arr))
+print(np.var(arr))
