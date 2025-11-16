120bec5fdbaa9f0735cfbef11ddaa43249cbf20a
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-11-16 14:07:28 +0530

    scatter plot

---
+++ matplotlib/p-06.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [5, 3, 9, 6, 2]
+plt.scatter(x, y)
+plt.xlabel('X values')
+plt.ylabel('Y values')
+plt.title('Simple Scatter Plot')
+plt.grid(True)
+plt.show()
