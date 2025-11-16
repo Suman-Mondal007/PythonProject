7ed6c3dd3912e2daf602cdf4406c9e0b8137615f
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-11-16 13:40:32 +0530

    simple book sale bar

---
+++ matplotlib/p-03.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=['Mon','Tues','Wed','Thur','Fri']
+y=[10,15,7,20,12]
+plt.bar(x,y)
+plt.title('Book sales data')
+plt.xlabel('Day')
+plt.ylabel('Book sales')
+plt.show()
