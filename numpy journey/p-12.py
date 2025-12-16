ccb671e6749929444b2d0b9416c1523ee503afbb
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2025-12-16 07:52:18 +0530

    fact

--- numpy journey/p-12.py
+++ numpy journey/p-12.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 n = 5
 fact = np.prod(np.arange(1, n + 1))
 print("Factorial:", fact)
---
+++ pandas journey/p-03.py
@@ -0,0 +1,7 @@
+a1, b1, c1 = 7, 2, 8
+a2, b2, c2 = 4, 5, 10
+D = a1*b2 - a2*b1
+x = (c1*b2 - c2*b1) / D
+y = (a1*c2 - a2*c1) / D
+print("Value of x =", x)
+print("Value of y =", y)
